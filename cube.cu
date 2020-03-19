1b0dfed27e225d3a40faaa245d2548dccc09e7ed
Tom <tom.sabu@students.iiit.ac.in>
2020-03-19 23:27:01 +0530

    Cube of numbers

--- cube.cu
+++ cube.cu
@@ -4,23 +4,21 @@
 #include <time.h>
 #include <stdio.h>
 
-// #define N 1<<20
-// #define THREADS 1024
-// #define BLOCKS 1024
 
-__global__ void square(float *d_out,float *d_in)
+
+__global__ void cube(float *d_out,float *d_in)
 {
 
     int id=threadIdx.x;
+    printf("Thread id  : %d\n",id);
     float f=d_in[id];
-    d_out=f*f;
+    d_out[id]=f*f*f;
 }
-
 
 int main()
 {
-    int AS=64;
-    int AB=AS*sizeof(float);
+    const int AS=96;
+    const int AB=AS*sizeof(float);
 
     float h_in[AS];
 
@@ -28,15 +26,16 @@ int main()
     h_in[i]=float(i);
 
     float h_out[AS];
-    float *d_in;
-    float *d_out;
+    float * d_in;
+    float * d_out;
     cudaMalloc((void **)&d_in,AB);
     cudaMalloc((void **)&d_out,AB);
     cudaMemcpy(d_in,h_in,AB,cudaMemcpyHostToDevice);
-    square<<<1,AS>>>(d_out,d_in);
+    //square<<<1,AS>>>(d_out,d_in);
+    cube<<<dim3(1,1,1),dim3(AS,1,1)>>>(d_out,d_in);
     cudaMemcpy(h_out,d_out,AB,cudaMemcpyDeviceToHost);
     for(int i=0;i<AS;i++)
-    cout<<h_out[i]<<",";
+    printf("%f,",h_out[i]);
     cudaFree(d_in);
     cudaFree(d_out);
     return 0;
